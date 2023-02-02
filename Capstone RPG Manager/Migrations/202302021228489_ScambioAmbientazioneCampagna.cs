92dd0cbc95da1362b9856cb8c09f8183dfae7450
Femnir81 <inmydefence81@yahoo.it>
2023-02-02 14:29:50 +0100

    a

---
+++ Capstone RPG Manager/Migrations/202302021228489_ScambioAmbientazioneCampagna.cs
@@ -0,0 +1,36 @@
+﻿namespace Capstone_RPG_Manager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ScambioAmbientazioneCampagna : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.CampagneTab", "IDUtentiTab", "dbo.UtentiTab");
+            DropIndex("dbo.AmbientazioniTab", new[] { "IDCampagneTab" });
+            DropIndex("dbo.CampagneTab", new[] { "IDUtentiTab" });
+            RenameColumn(table: "dbo.CampagneTab", name: "IDCampagneTab", newName: "IDAmbientazioniTab");
+            AddColumn("dbo.AmbientazioniTab", "IDUtentiTab", c => c.Int(nullable: false));
+            CreateIndex("dbo.AmbientazioniTab", "IDUtentiTab");
+            CreateIndex("dbo.CampagneTab", "IDAmbientazioniTab");
+            AddForeignKey("dbo.AmbientazioniTab", "IDUtentiTab", "dbo.UtentiTab", "ID");
+            DropColumn("dbo.AmbientazioniTab", "IDCampagneTab");
+            DropColumn("dbo.CampagneTab", "IDUtentiTab");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.CampagneTab", "IDUtentiTab", c => c.Int(nullable: false));
+            AddColumn("dbo.AmbientazioniTab", "IDCampagneTab", c => c.Int(nullable: false));
+            DropForeignKey("dbo.AmbientazioniTab", "IDUtentiTab", "dbo.UtentiTab");
+            DropIndex("dbo.CampagneTab", new[] { "IDAmbientazioniTab" });
+            DropIndex("dbo.AmbientazioniTab", new[] { "IDUtentiTab" });
+            DropColumn("dbo.AmbientazioniTab", "IDUtentiTab");
+            RenameColumn(table: "dbo.CampagneTab", name: "IDAmbientazioniTab", newName: "IDCampagneTab");
+            CreateIndex("dbo.CampagneTab", "IDUtentiTab");
+            CreateIndex("dbo.AmbientazioniTab", "IDCampagneTab");
+            AddForeignKey("dbo.CampagneTab", "IDUtentiTab", "dbo.UtentiTab", "ID");
+        }
+    }
+}
